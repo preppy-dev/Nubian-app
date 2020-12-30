604bfb9aa13e1b35835a8393f467b31ed3eb99bc
unknown <ninopreppy@gmail.com>
2020-12-30 17:26:15 -0300

    create node and express basic server and add data to fetching

---
+++ backend/data.js
@@ -0,0 +1,78 @@
+const data = {
+  products:[
+    {
+      _id:'1',
+      name:'Nike Slim Shirt',
+      Category:'Shirts',
+      image:'/images/p1.jpg',
+      price:120,
+      countInStock:0,
+      brand:'Nike',
+      rating:'4.5',
+      numReviews:'10',
+      description:'high quality product'
+    },
+    {
+      _id:'2',
+      name:'Adidas Slim Shirt',
+      Category:'Shirts',
+      image:'/images/p2.jpg',
+      price:140,
+      countInStock:2,
+      brand:'Adidas',
+      rating:'4.5',
+      numReviews:'11',
+      description:'high quality product'
+    },
+    {
+      _id:'3',
+      name:'Lacost Free Shirt',
+      Category:'Shirts',
+      image:'/images/p3.jpg',
+      price:120,
+      countInStock:1,
+      brand:'Lacost',
+      rating:'4.5',
+      numReviews:'8',
+      description:'high quality product'
+    },
+    {
+      _id:'4',
+      name:'Nike Slim Pant',
+      Category:'Pants',
+      image:'/images/p4.jpg',
+      price:120,
+      countInStock:4,
+      brand:'Nike',
+      rating:'4.5',
+      numReviews:'5',
+      description:'high quality product'
+    },
+    {
+      _id:'5',
+      name:'Puma Slim Pant',
+      Category:'Pants',
+      image:'/images/p5.jpg',
+      price:150,
+      countInStock:5,
+      brand:'Puma',
+      rating:'4.5',
+      numReviews:'15',
+      description:'high quality product'
+    },
+    {
+      _id:'6',
+      name:'Puma Slim Pant',
+      Category:'Pants',
+      image:'/images/p6.jpg',
+      price:100,
+      countInStock:5,
+      brand:'Puma',
+      rating:'4.5',
+      numReviews:'9',
+      description:'high quality product'
+    },
+  ]
+}
+
+export default data;
